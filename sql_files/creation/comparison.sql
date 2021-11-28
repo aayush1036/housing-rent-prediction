1822012092246a0bbea406dfef0875bb2ff113e7
aayush1036 <aayushmaan1306@gmail.com>
2021-11-29 01:28:18 +0530

    comlpete sql part

---
+++ sql_files/creation/comparison.sql
@@ -0,0 +1,35 @@
+USE PREPROCESS;
+
+CREATE TABLE preprocess_count(
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+city VARCHAR(20),
+nrows INTEGER
+);
+
+INSERT INTO preprocess_count (city, nrows) VALUES 
+('Ahemdabad', (SELECT COUNT(*) FROM AHEMDABAD)),
+('Bangalore', (SELECT COUNT(*) FROM BANGALORE)),
+('Chennai', (SELECT COUNT(*) FROM CHENNAI)),
+('Delhi', (SELECT COUNT(*) FROM DELHI)),
+('Hyderabad', (SELECT COUNT(*) FROM HYDERABAD)),
+('Kolkata', (SELECT COUNT(*) FROM KOLKATA)),
+('Mumbai', (SELECT COUNT(*) FROM MUMBAI)),
+('Pune', (SELECT COUNT(*) FROM PUNE));
+
+USE CLEAN;
+
+CREATE TABLE clean_count(
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+city VARCHAR(20),
+nrows INTEGER
+);
+
+INSERT INTO clean_count (city, nrows) VALUES 
+('Ahemdabad', (SELECT COUNT(*) FROM AHEMDABAD)),
+('Bangalore', (SELECT COUNT(*) FROM BANGALORE)),
+('Chennai', (SELECT COUNT(*) FROM CHENNAI)),
+('Delhi', (SELECT COUNT(*) FROM DELHI)),
+('Hyderabad', (SELECT COUNT(*) FROM HYDERABAD)),
+('Kolkata', (SELECT COUNT(*) FROM KOLKATA)),
+('Mumbai', (SELECT COUNT(*) FROM MUMBAI)),
+('Pune', (SELECT COUNT(*) FROM PUNE));
