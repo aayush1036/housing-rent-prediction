45908628972c20d5bf0b73517f6d2a4062e079ea
aayush1036 <aayushmaan1306@gmail.com>
2022-06-07 16:59:32 +0000

    Remove debug mode

--- main.py
+++ main.py
@@ -71,4 +71,4 @@ def contribute():
             return render_template('thanks.html', status='failure')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
